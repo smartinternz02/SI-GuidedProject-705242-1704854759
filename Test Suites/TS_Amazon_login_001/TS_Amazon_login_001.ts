4878a4479ecfa1698d54472a0a5dbf711d15b414
hemanth <hemanthkumarvadigi@gmail.com>
2024-01-23 20:52:34 +0530

    Data_Driven_Test

---
+++ Test Suites/TS_Amazon_login_001/TS_Amazon_login_001.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>DDT_Amazon_login_001 test case is used in the test suite</description>
+   <name>TS_Amazon_login_001</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>4d4c1e05-de49-4eb4-942b-3b815651bc3c</testSuiteGuid>
+   <testCaseLink>
+      <guid>d5fbf9ad-abb9-4c27-84da-8238cb81ddf5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DataDrivenTesting/DDT_Amazon_login_001</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>07871b25-0d28-4fa7-acf6-28383e241b81</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Test_Data/DDT_Amazon_login_Excel_file_001</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>07871b25-0d28-4fa7-acf6-28383e241b81</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>USERNAME</value>
+         <variableId>d2e28cc1-16a4-4b36-a30f-7c0d5b9e93e8</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>07871b25-0d28-4fa7-acf6-28383e241b81</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>PASSWORD</value>
+         <variableId>25af8b48-58c1-4860-8362-dce18c61d53b</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
